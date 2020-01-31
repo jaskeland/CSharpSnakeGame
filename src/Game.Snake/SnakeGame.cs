5b6317dc49418e644f687d78834fc22495e85439
Jonas Askeland <jonas.askeland@payex.com>
2020-01-31 12:37:58 +0100

    Refactor snake game map out to own class

--- src/Game.Snake/SnakeGame.cs
+++ src/Game.Snake/SnakeGame.cs
@@ -12,61 +12,42 @@ namespace Game.Snake
                 throw new ArgumentException("Map size must be greater than [0,0]", nameof(mapSize));
             }
 
-            MapSize = mapSize;
-            _map = new TileContent[mapSize.X, mapSize.Y];
+            _map = new SnakeMap(mapSize, randomSeed);
 
             _snakeBody = new SnakeBody(mapSize / 2);
-            _random = randomSeed == 0 ? new Random() : new Random(randomSeed);
 
-            SetTile(_snakeBody.Head, TileContent.SnakeHead);
+            _map.SetTile(_snakeBody.Head, TileContent.SnakeHead);
         }
-
-        public Vector2u MapSize { get; }
 
         public Vector2u Head => _snakeBody.Head;
 
-        public TileContent[,] Map => (TileContent[,])_map.Clone();
+        public TileContent[,] Map => _map.AsReadOnly();
 
-        private readonly TileContent[,] _map;
+        private readonly SnakeMap _map;
         private readonly SnakeBody _snakeBody;
-        private Random _random;
         private SnakeDirection _currentDirection;
 
         public void Update()
         {
-            if (!IsInsideBounds(_snakeBody.Head.AddSnakeDirection(_currentDirection)))
+            if (!_map.IsInsideBounds(_snakeBody.Head.AddSnakeDirection(_currentDirection)))
             {
                 return;
             }
 
-            SetTile(_snakeBody.Tail, TileContent.Empty);
+            _map.SetTile(_snakeBody.Tail, TileContent.Empty);
             _snakeBody.Move(_currentDirection);
 
             foreach (var segment in _snakeBody.BodySegments)
             {
-                SetTile(segment, TileContent.SnakeBody);
+                _map.SetTile(segment, TileContent.SnakeBody);
             }
 
-            SetTile(_snakeBody.Head, TileContent.SnakeHead);
+            _map.SetTile(_snakeBody.Head, TileContent.SnakeHead);
         }
 
         public void SetDirection(SnakeDirection direction)
         {
             _currentDirection = direction;
-        }
-
-        private void SetTile(Vector2u coordinate, TileContent content)
-        {
-            if (coordinate.X > MapSize.X || coordinate.Y > MapSize.Y)
-            {
-                return;
-            }
-            _map[coordinate.X, coordinate.Y] = content;
-        }
-
-        private bool IsInsideBounds(Vector2u coordinate)
-        {
-            return coordinate.X < MapSize.X && coordinate.Y < MapSize.Y;
         }
     }
 }
---
+++ src/Game.Snake/SnakeMap.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SFML.System;
+
+namespace Game.Snake
+{
+    public class SnakeMap
+    {
+        public SnakeMap(Vector2u mapSize, int randomSeed = 0)
+        {
+            MapSize = mapSize;
+            _random = randomSeed == 0 ? new Random() : new Random(randomSeed);
+            _map = new TileContent[mapSize.X, mapSize.Y];
+        }
+
+        public Vector2u MapSize { get; }
+
+        private readonly Random _random;
+        private readonly TileContent[,] _map;
+
+        public TileContent this[uint x, uint y]
+        {
+            get => _map[x, y];
+            set => _map[x, y] = value;
+        }
+
+        public TileContent[,] AsReadOnly()
+        {
+            return (TileContent[,])_map.Clone();
+        }
+
+        public void AddFoodAtRandomLocation(uint amountToAdd)
+        {
+            var hasEmptyTiles = _map.Cast<TileContent>().Any(tile => tile == TileContent.Empty);
+            if (!hasEmptyTiles)
+            {
+                return;
+            }
+
+            var emptySpots = GetCoordinatesForEmptyTiles().ToList();
+
+            for (var i = 0; i < amountToAdd; i++)
+            {
+                var nextEmptyIndex = _random.Next(0, emptySpots.Count - 1);
+                var newFoodCoordinate = emptySpots[nextEmptyIndex];
+
+                _map[newFoodCoordinate.X, newFoodCoordinate.Y] = TileContent.Food;
+                emptySpots.Remove(newFoodCoordinate);
+            }
+        }
+
+        public void SetTile(Vector2u coordinate, TileContent content)
+        {
+            if (coordinate.X > MapSize.X || coordinate.Y > MapSize.Y)
+            {
+                return;
+            }
+            _map[coordinate.X, coordinate.Y] = content;
+        }
+
+        public bool IsInsideBounds(Vector2u coordinate)
+        {
+            return coordinate.X < MapSize.X && coordinate.Y < MapSize.Y;
+        }
+
+        private IEnumerable<Vector2u> GetCoordinatesForEmptyTiles()
+        {
+            for (uint x = 0; x < MapSize.X; x++)
+            {
+                for (uint y = 0; y < MapSize.Y; y++)
+                {
+                    if (_map[x, y] == TileContent.Empty)
+                        yield return new Vector2u(x, y);
+                }
+            }
+        }
+    }
+}
